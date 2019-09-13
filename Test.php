e64bc258ae3cb37b70356efa3dd57d2e20f4e9cc
ShivaniMadhunala <55242792+ShivaniMadhunala@users.noreply.github.com>
2019-09-13 19:40:25 +0530

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	         <form action="displaydetails.php" method="post">
+            <label>Enter the username you want to Approve:</label><br>
+         <textarea cols="255" rows="1" name="comments" id="para1">
+                  </textarea><br>
+         <input type="submit" name="button" value="Submit"/></form>
+</body>
+</html>
