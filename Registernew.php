dfd61445161cf4c4db5b417a09afaad40442755c
ShivaniMadhunala <55242792+ShivaniMadhunala@users.noreply.github.com>
2019-09-13 19:39:38 +0530

    Create Registernew.php

---
+++ Registernew.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+</head>
+<body>
+    <div class="col-lg-6">
+        <h2>New Account Sign up/Register</h2>
+        <form action="registration.php" method="post">
+          <div class="form-group">
+            <label> UserName </label>
+            <input type="text" name="user" class="form-control">
+          </div>
+          <div class="form-group">
+            <label> Password </label>
+            <input type="Password" name="password" class="form-control">
+          </div>
+          <button type="submit" class="btn btn-primary">Sign in</button>
+        </form>  
+    </div>
+</body>
+</html>
+
